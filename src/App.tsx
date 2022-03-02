659db45ee148e4a430c7f2977d856e9090b87a90
Júlio Macedo <juliocesarmacedo@icloud.com>
2022-03-02 19:05:12 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 
+Modal.setAppElement('#root') //recomendação da doc do react-modal, para que ele seja renderizado dentro da root e não dentro do body, como ele ficaria sem executar essa função.
+
 export function App() {
+  //MODAL (TRANSAÇÕES) -> utilizando a lib react-modal 
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false)
+  }
+
+  //aqiu
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} 
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api' //significa dizer que todas as chamadas a serem feitas pelo mirage vão estã A PARTIR do "/api" -> Observar o endereço no useEffect do TransactionsTable
+
+    this.get('/transactions', () =>{ //qdo houver uma chamada do tipo "GET" para "api/transactions", essa chamava vai devolver o que estiver demandado na função 
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
